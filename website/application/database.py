2bca72c8194db1ca86d4d7eb819d65720e386911
Matthew Tchouikine <107216405+matt4tch@users.noreply.github.com>
2023-08-18 16:48:33 -0700

    Added more to blueprint of the app

--- website/application/database.py
+++ website/application/database.py
@@ -1,2 +1,5 @@
+import sqlite3
+from sqlite3 import Error
+
 class DataBase():
     pass
--- website/main.py
+++ website/main.py
@@ -7,3 +7,22 @@ import config
 
 app = create_app()
 socketio = SocketIO(app)
+
+#@socketio.on('event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    """
+    handles saving messages once received from web server and
+    sending message to other clients
+    :param json: json
+    :param methods: POST GET
+    :return: None
+    """
+    data = dict(json)
+    if "name" in data:
+        db = DataBase()
+        db.save_message(data["name"], data["message"])
+
+handle_my_custom_event = socketio.on('event')(handle_my_custom_event)
+
+if __name__ == "__main__":
+    pass
